5cf18c266c1af08a78cb56961fdf382026c8e0f5
wangj <wangjianweb@sijibao.com>
2022-06-06 10:57:19 +0800

    fix: 复杂类型不解构

--- dist/removeRef.js
+++ dist/removeRef.js
@@ -9,39 +9,21 @@ const circleStub = {
 export function removeRef(swagger) {
     Object.keys(swagger.definitions).forEach((key) => {
         stackRefs = [];
-        swagger.definitions[key] = removeWrapper(removeRefObjectScheme(swagger, swagger.definitions[key]));
+        swagger.definitions[key] = removeWrapper(removeRefObjectScheme(swagger, swagger.definitions[key]), swagger);
     });
 }
-export function removeWrapper(objectScheme) {
-    var _a, _b, _c;
+export function removeWrapper(objectScheme, swagger) {
+    var _a, _b, _c, _d, _e;
     if ((objectScheme === null || objectScheme === void 0 ? void 0 : objectScheme.type) === "object" &&
         ((_a = objectScheme.properties) === null || _a === void 0 ? void 0 : _a.code) &&
         (((_b = objectScheme.properties) === null || _b === void 0 ? void 0 : _b.msg) || ((_c = objectScheme.properties) === null || _c === void 0 ? void 0 : _c.message))) {
+        if ((_d = objectScheme.properties.data) === null || _d === void 0 ? void 0 : _d.$ref) {
+            return swagger.definitions[convertRefKey((_e = objectScheme.properties.data) === null || _e === void 0 ? void 0 : _e.$ref)];
+        }
         return objectScheme.properties.data;
     }
     return objectScheme;
 }
 function removeRefObjectScheme(swagger, objectScheme) {
-    if (!objectScheme) {
-        return objectScheme;
-    }
-    stackRefs.push(objectScheme);
-    if (objectScheme.$ref) {
-        const target = swagger.definitions[convertRefKey(objectScheme.$ref)];
-        if (stackRefs.includes(target)) {
-            return circleStub;
-        }
-        return target;
-    }
-    if (objectScheme.type === "object" && objectScheme.properties) {
-        Object.keys(objectScheme.properties).forEach((p) => {
-            if (objectScheme.properties && objectScheme.properties[p]) {
-                objectScheme.properties[p] = removeRefObjectScheme(swagger, objectScheme.properties[p]);
-            }
-        });
-    }
-    else if (objectScheme.type === "array" && objectScheme.items) {
-        objectScheme.items = removeRefObjectScheme(swagger, objectScheme.items);
-    }
     return objectScheme;
 }
--- src/removeRef.ts
+++ src/removeRef.ts
@@ -13,17 +13,26 @@ export function removeRef(swagger: Swagger) {
   Object.keys(swagger.definitions).forEach((key) => {
     stackRefs = [];
     swagger.definitions[key] = removeWrapper(
-      removeRefObjectScheme(swagger, swagger.definitions[key])
+      removeRefObjectScheme(swagger, swagger.definitions[key]),
+      swagger
     );
   });
 }
 
-export function removeWrapper(objectScheme: ObjectScheme): ObjectScheme {
+export function removeWrapper(
+  objectScheme: ObjectScheme,
+  swagger: Swagger
+): ObjectScheme {
   if (
     objectScheme?.type === "object" &&
     objectScheme.properties?.code &&
     (objectScheme.properties?.msg || objectScheme.properties?.message)
   ) {
+    if (objectScheme.properties.data?.$ref) {
+      return swagger.definitions[
+        convertRefKey(objectScheme.properties.data?.$ref)
+      ];
+    }
     return objectScheme.properties.data;
   }
   return objectScheme;
@@ -33,29 +42,30 @@ function removeRefObjectScheme(
   swagger: Swagger,
   objectScheme: ObjectScheme
 ): ObjectScheme {
-  if (!objectScheme) {
-    return objectScheme;
-  }
-  stackRefs.push(objectScheme);
-  if (objectScheme.$ref) {
-    // return removeRefObjectScheme(swagger, objectScheme.$ref);
-    const target = swagger.definitions[convertRefKey(objectScheme.$ref)];
-    if (stackRefs.includes(target)) {
-      return circleStub;
-    }
-    return target;
-  }
-  if (objectScheme.type === "object" && objectScheme.properties) {
-    Object.keys(objectScheme.properties).forEach((p) => {
-      if (objectScheme.properties && objectScheme.properties[p]) {
-        objectScheme.properties[p] = removeRefObjectScheme(
-          swagger,
-          objectScheme.properties[p]
-        );
-      }
-    });
-  } else if (objectScheme.type === "array" && objectScheme.items) {
-    objectScheme.items = removeRefObjectScheme(swagger, objectScheme.items);
-  }
+  // if (!objectScheme) {
+  //   return objectScheme;
+  // }
+  // stackRefs.push(objectScheme);
+  // if (objectScheme.$ref) {
+  //   // return removeRefObjectScheme(swagger, objectScheme.$ref);
+  //   const target = swagger.definitions[convertRefKey(objectScheme.$ref)];
+  //   if (stackRefs.includes(target)) {
+  //     console.log(target);
+  //     return circleStub;
+  //   }
+  //   return target;
+  // }
+  // if (objectScheme.type === "object" && objectScheme.properties) {
+  //   Object.keys(objectScheme.properties).forEach((p) => {
+  //     if (objectScheme.properties && objectScheme.properties[p]) {
+  //       objectScheme.properties[p] = removeRefObjectScheme(
+  //         swagger,
+  //         objectScheme.properties[p]
+  //       );
+  //     }
+  //   });
+  // } else if (objectScheme.type === "array" && objectScheme.items) {
+  //   objectScheme.items = removeRefObjectScheme(swagger, objectScheme.items);
+  // }
   return objectScheme;
 }
