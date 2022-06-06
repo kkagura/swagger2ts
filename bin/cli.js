5975a282f8eebe8748f7e6b5872b83f5f731f600
wangj <wangjianweb@sijibao.com>
2022-06-06 11:22:23 +0800

    feat: 输出版本号

--- bin/cli.js
+++ bin/cli.js
@@ -2,11 +2,12 @@
 
 import parser from "yargs-parser";
 import { run } from "../dist/index.js";
+import fs from "fs-extra";
+import path from "path";
 
-const HELP = `Usage
-  $ swagger2ts [options]
-
+const HELP = `
 Options
+  --version, -v                输出版本号
   --help, -h                   用法参考
   --tag, -t                    开启筛选
   --no-mock, -n                不生成模拟数据
@@ -14,21 +15,29 @@ Options
 
 const [, , ...args] = process.argv;
 const flags = parser(args, {
-  boolean: ["help", "tag", "no-mock"],
+  boolean: ["help", "tag", "no-mock", "version"],
   number: [],
   alias: {
     help: ["h"],
     tag: ["t"],
     "no-mock": ["n"],
+    version: ["v"],
   },
 });
-if (flags._.length !== 2) {
-  console.log("参数错误，请运行 npx wyyt-swagger-to-ts -h 查看参数列表");
-  process.exit(0);
-}
 
 if (flags.help || flags.h) {
   console.log(HELP);
+  process.exit(0);
+}
+if (flags.version) {
+  const packageJson = JSON.parse(
+    fs.readFileSync(new URL("../package.json", import.meta.url), "utf8")
+  );
+  console.log(packageJson.version);
+  process.exit(0);
+}
+if (flags._.length !== 2) {
+  console.log("参数错误，请运行 npx wyyt-swagger-to-ts -h 查看参数列表");
   process.exit(0);
 }
 const [i, o] = flags._;
